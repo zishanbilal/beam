9569729d25f6a153e9b6ab2cf07fe0d008c5953c
Zeeshan Bilal <zeeshan.bilal@northstar-its.com>
2017-07-28 00:08:28 +0500

    Test refactoring

--- src/test/java/beam/util/ConfigUtilTest.java
+++ src/test/java/beam/util/ConfigUtilTest.java
@@ -4,19 +4,27 @@ import beam.sim.config.BeamConfig;
 import org.junit.Test;
 
 public class ConfigUtilTest {
+
+    public static final String BASE_DATE = "2016-10-17T00:00:00-07:00";
+
     @Test
     public void testBuildDefaultConfig(){
         BeamConfig cfg = beam.util.ConfigUtil.buildDefaultConfig();
-        assert (cfg.beam().routing().baseDate().equalsIgnoreCase("2016-10-17T00:00:00-07:00"));
+        assert (BASE_DATE.equalsIgnoreCase(cfg.beam().routing().baseDate()));
     }
 
     @Test
     public void testBuildDefaultRouting(){
-        assert (beam.util.ConfigUtil.buildDefaultRouting().baseDate().equalsIgnoreCase("2016-10-17T00:00:00-07:00"));
+        assert (BASE_DATE.equalsIgnoreCase(beam.util.ConfigUtil.buildDefaultRouting().baseDate()));
     }
 
     @Test
     public void testBuildRoutingWithBaseDate(){
-        assert (beam.util.ConfigUtil.buildRoutingWithBaseDate("2016-10-17T00:00:00-07:00").baseDate().equalsIgnoreCase("2016-10-17T00:00:00-07:00"));
+        assert (BASE_DATE.equalsIgnoreCase(beam.util.ConfigUtil.buildRoutingWithBaseDate(BASE_DATE).baseDate()));
+    }
+
+    @Test
+    public void testBuildRoutingWithBaseDate_Failuer(){
+        assert (!BASE_DATE.equalsIgnoreCase(beam.util.ConfigUtil.buildRoutingWithBaseDate(null).baseDate()));
     }
 }
