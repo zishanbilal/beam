a6199cb61a96c45091367227ce353f52acef4d3e
mr-asif-nor <muhammad.asif1.northstar@gmail.com>
2017-11-30 12:07:10 +0500

    Implemented the test case in RouterTest to receive all the logging messages for the Router from receive behavior.

--- src/main/scala/beam/utils/AkkaMessageLogger.scala
+++ src/main/scala/beam/utils/AkkaMessageLogger.scala
@@ -1,35 +1,67 @@
 package beam.utils
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.event.Logging._
 
+import scala.collection.mutable
 
 class AkkaMessageLogger extends Actor {
 
-  //val mon = context.actorOf(Props[MonitorEndpoint], "MonitorEndpoint")
-
   def receive = {
-
 
     case InitializeLogger(bus) => sender ! LoggerInitialized
     case Error(cause, logSource, logClass, message: String) =>
-      //println(">> err -> " + message)
-      println(format(logSource, message, Console.RED))
-    //mon ! format(logSource, message, Console.RED)
+    //println(format(logSource, message, Console.RED))
     case Warning(logSource, logClass, message: String) =>
-      //println(">>warn -> " + message)
-      println(format(logSource, message, Console.YELLOW))
+    //println(format(logSource, message, Console.YELLOW))
     //mon ! format(logSource, message, Console.YELLOW)
     case Info(logSource, logClass, message: String) =>
-      //println(">>info -> " + message)
-      println(format(logSource, message, Console.GREEN))
+    //println(format(logSource, message, Console.GREEN))
     //mon ! format(logSource, message, Console.GREEN)
     case Debug(logSource, logClass, message: String) =>
       //println(">>debug -> " + message)
       println(format(logSource, message, Console.GREEN))
+      AkkaMessageLogger.collect(logSource, logClass, message)
     //mon ! format(logSource, message, Console.GREEN)
+    case AkkaMessageLogger.GET_LIST => {
+      println("Printing the list before sending it back to " + sender.path + " - " + sender)
+      sender ! AkkaMessageLogger.debugMessages
+    }
   }
 
   def format(src: String, msg: String, code: String) = s"${Console.BLUE}R> $code[${src.split('/').last}] ${Console.RESET}$msg"
 }
 
+object AkkaMessageLogger{
+
+  val MAP_KEY_RECEIVED_HANDLED: String = "received handled message"
+  case object GET_LIST
+
+  val debugMessages: mutable.Map[String, mutable.MutableList[DebugMessage]] = mutable.Map()
+
+
+  def collect(logSource: String, logClass: Class[_], message: String): Unit = {
+    if (message.contains(MAP_KEY_RECEIVED_HANDLED)) {
+
+      val mParts = message.split(" from ")
+      val dm = DebugMessage(mParts(0).split(MAP_KEY_RECEIVED_HANDLED)(1).trim, mParts(1), logSource, logClass.getName)
+
+      collect(dm)
+    }
+  }
+
+  def collect(m: DebugMessage): Unit = {
+
+    if(debugMessages.contains(MAP_KEY_RECEIVED_HANDLED) )
+      debugMessages(MAP_KEY_RECEIVED_HANDLED) += m
+    else{
+      debugMessages += (MAP_KEY_RECEIVED_HANDLED  -> mutable.MutableList(m))
+    }
+  }
+
+  def printList(args: TraversableOnce[_]): Unit = {
+    args.foreach(println)
+  }
+}
+
+case class DebugMessage(message: String, sender: String, sourceActor: String, logActor: String)
---
+++ src/test/scala/beam/integration/RouterTest.scala
@@ -0,0 +1,155 @@
+package beam.integration
+
+
+
+import java.util
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration._
+import akka.pattern.ask
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.util.Timeout
+import beam.agentsim.events.PathTraversalEvent
+import beam.agentsim.scheduler.BeamAgentScheduler
+import beam.agentsim.scheduler.BeamAgentScheduler.SchedulerProps
+import beam.physsim.jdeqsim.AgentSimToPhysSimPlanConverter
+import beam.physsim.jdeqsim.akka.{AkkaEventHandlerAdapter, EventManagerActor, JDEQSimActor}
+import beam.router.BeamRouter.UpdateTravelTime
+import beam.sim._
+import beam.sim.config.{BeamConfig, BeamLoggingSetup, ConfigModule}
+import beam.sim.controler.BeamControler
+import beam.sim.controler.corelisteners.{BeamControllerCoreListenersModule, BeamPrepareForSimImpl}
+import beam.sim.modules.{AgentsimModule, BeamAgentModule, UtilsModule}
+import beam.utils.{AkkaMessageLogger, DebugMessage, FileUtils}
+import beam.utils.reflection.ReflectionUtils
+import com.conveyal.r5.streets.StreetLayer
+import glokka.Registry
+import glokka.Registry.Created
+import org.matsim.api.core.v01.{Id, Scenario}
+import org.matsim.api.core.v01.events.Event
+import org.matsim.api.core.v01.network.Link
+import org.matsim.api.core.v01.population._
+import org.matsim.core.api.experimental.events.EventsManager
+import org.matsim.core.config.{Config, ConfigUtils}
+import org.matsim.core.controler.events.{IterationEndsEvent, IterationStartsEvent, ShutdownEvent}
+import org.matsim.core.controler.listener.{IterationEndsListener, IterationStartsListener, ShutdownListener}
+import org.matsim.core.controler.{AbstractModule, ControlerI, NewControlerModule, PrepareForSim}
+import org.matsim.core.events.EventsUtils
+import org.matsim.core.events.handler.{BasicEventHandler, EventHandler}
+import org.matsim.core.mobsim.jdeqsim.JDEQSimConfigGroup
+import org.matsim.core.population.routes.RouteUtils
+import org.matsim.core.scenario.{ScenarioByInstanceModule, ScenarioUtils}
+import org.matsim.core.utils.collections.Tuple
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{MustMatchers, WordSpecLike}
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
+
+class RouterTest
+  extends TestKit(ActorSystem("beam-actor-system"))
+    //extends RunBeam
+    with RunBeam
+    with MustMatchers with WordSpecLike with ImplicitSender with MockitoSugar
+    with ShutdownListener with IterationEndsListener{
+
+  val __this_instance = this
+
+  var services: BeamServices = _
+  val eventsManager: EventsManager = EventsUtils.createEventsManager()
+  /////////////////////////
+  // These class variables need to be in place before runBeamWithConfigFile method is called
+  implicit val timeout = Timeout(50000, TimeUnit.SECONDS)
+  val log = LoggerFactory.getLogger(classOf[RouterTest])
+  ////////////////////////////////
+
+  override def beamInjector(scenario: Scenario,  matSimConfig: Config,mBeamConfig: Option[BeamConfig] = None): com.google.inject.Injector =
+    org.matsim.core.controler.Injector.createInjector(matSimConfig, AbstractModule.`override`(ListBuffer(new AbstractModule() {
+
+      override def install(): Unit = {
+        // MATSim defaults
+        install(new NewControlerModule)
+        install(new ScenarioByInstanceModule(scenario))
+        install(new controler.ControlerDefaultsModule)
+        install(new BeamControllerCoreListenersModule)
+
+        // Beam Inject below:
+        install(new ConfigModule)
+        install(new AgentsimModule)
+        install(new BeamAgentModule)
+        install(new UtilsModule)
+      }
+    }).asJava, new AbstractModule() {
+      override def install(): Unit = {
+        // Override MATSim Defaults
+        bind(classOf[PrepareForSim]).to(classOf[BeamPrepareForSimImpl])
+        // Beam -> MATSim Wirings
+        bindMobsim().to(classOf[BeamMobsim]) //TODO: This will change
+
+        addControlerListenerBinding().toInstance(__this_instance)
+        addControlerListenerBinding().to(classOf[BeamSim])
+
+        bind(classOf[EventsManager]).toInstance(eventsManager)
+        bind(classOf[ControlerI]).to(classOf[BeamControler]).asEagerSingleton()
+        mBeamConfig.foreach(beamConfig => bind(classOf[BeamConfig]).toInstance(beamConfig)) //Used for testing - if none passed, app will use factory BeamConfig
+
+
+      }
+    }))
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  override def notifyShutdown(event: ShutdownEvent): Unit = {
+
+    testRouterMessages
+  }
+
+  override def notifyIterationEnds(event: IterationEndsEvent): Unit = {
+    //testRouterMessages
+  }
+  //
+
+  def testRouterMessages = {
+    implicit val timeout = Timeout(50000, TimeUnit.SECONDS)
+
+    try {
+
+      val messageCollector = system.actorOf(Props(classOf[AkkaMessageLogger]))
+      println("messageCollector - From RouterTest -> " + messageCollector.path)
+
+      // Test cases
+      messageCollector.tell(AkkaMessageLogger.GET_LIST, testActor)
+
+      "AkkaMessageLogger" must {
+        "return a list of debug messages " in {
+          within(50000 millis) {
+            expectMsgPF() {
+              case l: mutable.MutableList[DebugMessage] => {
+
+                //log.info("DebugMessage List Received")
+                println("DebugMessage List Received: " + l.size)
+                AkkaMessageLogger.printList(l)
+
+              }
+              case o: Any => {
+                println(o)
+              }
+            }
+          }
+        }
+      }
+
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+  }
+
+  //
+  rumBeamWithConfigFile(Option("test/input/beamville/beam.conf"))
+}
+
